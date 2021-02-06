478e05a0f9979a7666b02390d831a6b843c71f91
mabrah381 <meril_abraham@comcast.com>
2021-02-06 16:18:11 +0530

    Big O

---
+++ data_structures/BigO/1.py
@@ -0,0 +1,12 @@
+nemo = ['nemo']
+
+
+def find_nemo(array):
+    for i in range(len(array)):
+        if array[i] == 'nemo':
+            print(array[i])
+
+
+if __name__ == '__main__':
+    find_nemo(nemo) # O(n) -> Linear Time
+
